672c0cb3d70945fa719a896f0e11e724a92f9737
DanielAlonsoTM <danielinogordo@gmail.com>
2020-06-01 02:47:21 -0400

    Instructions support is added

--- src/main/kotlin/com/smauraitech/spring_glados/services/DaoLayer.kt
+++ src/main/kotlin/com/smauraitech/spring_glados/services/DaoLayer.kt
@@ -1,6 +1,7 @@
 package com.smauraitech.spring_glados.services
 
 import com.smauraitech.spring_glados.models.Device
+import com.smauraitech.spring_glados.models.Instruction
 import com.smauraitech.spring_glados.models.Room
 import org.springframework.data.mongodb.repository.MongoRepository
 
@@ -8,4 +9,8 @@ interface RoomDAO : MongoRepository<Room, String> {
     fun findByName(name: String): List<Room>
 }
 
-interface DeviceDAO : MongoRepository<Device, String>
+interface DeviceDAO : MongoRepository<Device, String> {
+    fun findByRoomId(roomId: Int): List<Device>
+}
+
+interface InstructionDAO : MongoRepository<Instruction, String>
